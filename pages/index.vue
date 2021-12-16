4377fcfddd7789509ed6ffc9055e98b357e48727
dirghayusa <dikaptrw@gmail.com>
2021-12-16 19:42:42 +0800

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- navigation -->
+    <NavigationMain />
+
+    <!-- content -->
+    <div>
+      content
+    </div>
+    
+    <!-- footer -->
+    <FooterMain />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+}
+</script>
